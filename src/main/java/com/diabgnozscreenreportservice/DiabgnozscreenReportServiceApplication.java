445510120ff7bea677013a502ac18ae5c136fc06
Loïc Stringer <loic.stringer@gmail.com>
2021-05-19 11:25:30 +0200

    Initial commit

---
+++ src/main/java/com/diabgnozscreenreportservice/DiabgnozscreenReportServiceApplication.java
@@ -0,0 +1,13 @@
+package com.diabgnozscreenreportservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiabgnozscreenReportServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiabgnozscreenReportServiceApplication.class, args);
+	}
+
+}
